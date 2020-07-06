c3d343c55ff8c0e40219ae8ab4800484ec35ebbd
Ariel Cuesta <arielcc88@gmail.com>
2020-07-06 17:30:46 -0500

    employee parent class defined.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,28 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  //parent class constructor
+  constructor(id, name, email) {
+    //setting class attributes
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+
+  /*--------
+    METHODS
+    --------*/
+  //get employee's ID
+  getID() {
+    return this.id;
+  }
+
+  //get employee's name
+  getName() {
+    return this.name;
+  }
+
+  //get employee's email
+  getEmail() {
+    return this.email;
+  }
+}
